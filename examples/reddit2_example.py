c1b2d8116d84909b0863523e2066ee757de3c3b9
Pyrrxa <pyrrxa@gmail.com>
2018-10-06 16:20:22 -0400

    Added Delete Account Catch

--- examples/reddit2_example.py
+++ examples/reddit2_example.py
@@ -5,25 +5,26 @@ from requests import Session
 def main():
     graph_attri = {'overlap': 'false', 'spines': 'ortho', 'nodesep': '0.5'}
     node_attri = {'shape': 'circle', 'color': 'green'}
-    dot = Graph('G', filename = 'test.gv', engine = 'fdp', node_attr = node_attri, graph_attr = graph_attri)
+    dot = Graph('G', filename = 'test.gv', engine = 'twopi', node_attr = node_attri, graph_attr = graph_attri)
 
     reddit = praw.Reddit(user_agent='HackNCfysix',
              client_secret='Y0aO7fFQsiwDdx7mmQDyTxWwkzk',
              client_id = '7I5stAk1fFpC-Q')
     
     source = 'AskReddit'
-    top_posts = reddit.subreddit(source).top(limit=100)
+    top_posts = reddit.subreddit('askreddit').new(limit=100)
     dot.node(source, color='red')
     for post in top_posts:
+        
         user = post.author
-        for comment in user.comments.new(limit=10):
-            if comment.score < 5:
-                pass
-            else:
+        try:
+            for comment in user.comments.new(limit=10):
                 y = comment.subreddit.display_name
                 if y != source:
                     dot.node(y, color = 'blue')
                     dot.edge(source, y, color = 'orange')
+        except:
+            print("NoneType Caught")
 
 
     dot.view()
