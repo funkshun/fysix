9bb5650c27d7a1fbc5290c700c6771f70eb4fe49
TCRichards <thomas.c.richards@outlook.com>
2018-10-07 10:49:59 -0400

    Finished product

--- main/graphics.py
+++ main/graphics.py
@@ -10,270 +10,83 @@ from bokeh.plotting import figure, output_file, show, ColumnDataSource, gridplot
 from bokeh.models.glyphs import MultiLine
 from bokeh.models.markers import Circle
 from bokeh.colors import RGB
+from PIL import Image
 
-pickles = ["smallpolitical", "political", "entertainment"]
 
 ## Bokeh initialization code
 hv.extension('bokeh')
-output_file('DataVis.html')
+output_file('Reddit Data Modelling.html')
 
-# Code to launch on startup
-def startup():
-    infected = []
-    sub_reference = []
-    connections = []
-    subreddits = []
-    for pickle in pickles:
-        sim_tuple = simulator.simulate(pickle)
-        connections.append(sim_tuple[0])
-        subreddits.append(sim_tuple[1])
-        sub_reference.append(sim_tuple[2])
+def main():
+    summary = open('hacknc2018.html', 'r')
 
-    launch(subreddits, connections, sub_reference)
+    gifs = """
+    <!DOCTYPE html>
+    <html>
+    <body>
+    <p style = "position:absolute; TOP:50px; LEFT: 500px; WIDTH:600px">Our model predicts how interconnected subreddits might share ideas.
+    In the example below, the subreddit "The_Mueller" starts with an idea. Each subreddit has a chance of "conracting" the idea based on the strengths
+    of its conncetions and the concentration of the idea in the surrounding populations.
+    </p>
 
+    <img src = 'network.gif', width = 600px, height = 600px,
+    IMG STYLE="position:absolute; TOP:100px; LEFT:500px; WIDTH:600px; HEIGHT:600px">
 
-def launch(all_subreddits, all_connections, sub_reference):
-    # Matrices containing constants for each pickles
+    <img src = 'legend.png', width = 400px, height = 75px,
+    IMG STYLE="position:absolute; TOP:720px; LEFT:500px; HEIGHT:75px">
 
-    numPickles = len(pickles)
+    <img src = 'smallpolitical_plotloop.gif', width = 600px, height = 400px,
+    IMG STYLE="position:absolute; TOP:900px; LEFT:500px; WIDTH:600px; HEIGHT:600px">
 
-    # Store the position, number of subreddits, a
-    X = []
-    Y = []
-    R = []
+    <img src = 'gaming_plotloop.gif', width = 600px, height = 400px,
+    IMG STYLE="position:absolute; TOP:1500px; LEFT:500px; WIDTH:600px; HEIGHT:600px">
 
-    for i in range(numPickles):
-        X.append([])
-        Y.append([])
-        R.append([])
+    <img src = 'enter.gif', width = 600px, height = 400px,
+    IMG STYLE="position:absolute; TOP:2100px; LEFT:500px; WIDTH:600px; HEIGHT:600px">
 
-    N = []
+    <img src = 'political_plotloop.gif', width = 600px, height = 400px,
+    IMG STYLE="position:absolute; TOP:2700px; LEFT:500px; WIDTH:600px; HEIGHT:600px">
 
-    infected = all_subreddits
-    colors = []
-    for i in range(numPickles):
-        colors.append(make_color(infected[i]))
+    </body>
+    </html>
+    """
 
-    ## Unload parameters #######################################################
-    for l in range(len(all_subreddits)):
-        subreddits = all_subreddits[l]
-        connections = all_connections[l]
-        total = len(connections) # Number of SubReddits Investigated
-        timeSteps = len(subreddits[0]) # Length of the arrays (number of time steps)
-        x = np.zeros(total) # x Position
-        y = np.zeros(total) # y position
-        r = np.zeros(total) # radius of node
+    memeimages = """
+    <!DOCTYPE html>
+    <html>
+    <body>
 
-        for i in range(total): # For each sub reddit
-            x[i] = np.random.randint(10) # point's x value
-            y[i] = np.random.randint(10) # point's y value
-            r[i] = np.random.randint(40, 60) # radius
-            #subs[i] = np.random.randint(40, 60) # radius
+    <p style = "position:absolute; TOP:50px; LEFT: 1200px; WIDTH:400px"> We gathered some Reddit
+    user data to show how viral trends looks in the real world.   The below plots show that phenomena
+    such as memes experience massive surges at different times in different communities, giving
+    some credence to our model.
+    </p>
 
-        #Add the current subreddit data to the Matrices
-        X[l] = x
-        Y[l] = y
-        R[l] = r
-        N.append(total)
+    <img src = 'dankmemes.png', width = 200, height = 200, align = "right",
+    IMG STYLE="position:absolute; TOP:150px; LEFT:1200px; WIDTH:400px; HEIGHT:200px" >
 
-        ## Plotting code ########################################################
+    <img src = 'MemeEconomy.png', width = 200, height = 200, align = "right",
+    IMG STYLE="position:absolute; TOP:400px; LEFT:1200px; WIDTH:400px; HEIGHT:200px">
 
-    xdr = DataRange1d()
-    ydr = DataRange1d()
+    <img src = 'mothmemes.png', width = 200, height = 200, align = "right",
+    IMG STYLE="position:absolute; TOP:650   px; LEFT:1200px; WIDTH:400px; HEIGHT:200px">
 
-    curPickle = 0
+    </body>
+    </html>
+    """
 
-    source0 = ColumnDataSource(dict(x=X[0], y=Y[0],
-        r=R[0], colors=colors[0], name=sub_reference[0]))
+    description = Div(text = summary.read())
 
-    source1 = ColumnDataSource(dict(x=X[1], y=Y[1],
-        r=R[1], colors=colors[1], name=sub_reference[1]))
+    gifs = Div(text = gifs)
 
-    source2 = ColumnDataSource(dict(x=X[2], y=Y[2],
-        r=R[2], colors=colors[2], name=sub_reference[2]))
-
-#    source3= ColumnDataSource(dict(x=X[3], y=Y[3],
-#        r=R[3], colors=colors[3], name=sub_reference[3]))
-
-
-    plot0 = figure(
-        title=None, x_range=xdr, y_range=ydr, plot_width=300, plot_height=250,
-        h_symmetry=False, v_symmetry=False, min_border=0, toolbar_location = None)
-    plot0.axis.visible = None
-    plot0.xgrid.visible = False
-    plot0.ygrid.visible = False
-
-    plot0.add_tools(HoverTool(
-    tooltips = [
-            ('Sub-Reddit Name', '@name'),
-            ('URL', '@URL'),
-            ]
-        ))
-
-
-    # Add lines connecting each glyph
-    for i in range(N[curPickle]):
-        for j in range(N[curPickle]):
-            if j <= i:
-                rgbval = 255 - 255*connections[i][j] # Shade from white to black based on connection strength
-                lineColor = RGB(rgbval, rgbval, rgbval)
-                width = connections[i][j]
-                x = X[curPickle]; y = Y[curPickle]
-                plot0.line([x[i], x[j]], [y[i], y[j]],
-                    line_width = np.random.randint(7), line_color = lineColor)
-
-
-    # Add all the glyphs to the main plot
-    glyph = Circle(x="x", y="y", size="r", fill_color="colors", line_width=1)
-    plot0.add_glyph(source0, glyph)
-
-
-    plot1 = figure(
-        title=None, x_range=xdr, y_range=ydr, plot_width=300, plot_height=250,
-        h_symmetry=False, v_symmetry=False, min_border=0, toolbar_location = None)
-    plot1.axis.visible = None
-    plot1.xgrid.visible = False
-    plot1.ygrid.visible = False
-
-    plot1.add_tools(HoverTool(
-    tooltips = [
-            ('Sub-Reddit Name', '@name'),
-            ('URL', '@URL'),
-            ]
-        ))
-
-
-    # Add lines connecting each glyph
-    for i in range(N[curPickle]):
-        for j in range(N[curPickle]):
-            if j <= i:
-                rgbval = 255 - 255*connections[i][j] # Shade from white to black based on connection strength
-                lineColor = RGB(rgbval, rgbval, rgbval)
-                width = connections[i][j]
-                x = X[curPickle]; y = Y[curPickle]
-                plot1.line([x[i], x[j]], [y[i], y[j]],
-                    line_width = np.random.randint(7), line_color = lineColor)
-
-
-    # Add all the glyphs to the main plot
-    glyph = Circle(x="x", y="y", size="r", fill_color="colors", line_width=1)
-    plot1.add_glyph(source1, glyph)
-
-
-    plot2 = figure(
-        title=None, x_range=xdr, y_range=ydr, plot_width=300, plot_height=250,
-        h_symmetry=False, v_symmetry=False, min_border=0, toolbar_location = None)
-    plot2.axis.visible = None
-    plot2.xgrid.visible = False
-    plot2.ygrid.visible = False
-
-    plot2.add_tools(HoverTool(
-    tooltips = [
-            ('Sub-Reddit Name', '@name'),
-            ('URL', '@URL'),
-            ]
-        ))
-
-
-    # Add lines connecting each glyph
-    for i in range(N[curPickle]):
-        for j in range(N[curPickle]):
-            if j <= i:
-                rgbval = 255 - 255*connections[i][j] # Shade from white to black based on connection strength
-                lineColor = RGB(rgbval, rgbval, rgbval)
-                width = connections[i][j]
-                x = X[curPickle]; y = Y[curPickle]
-                plot2.line([x[i], x[j]], [y[i], y[j]],
-                    line_width = np.random.randint(7), line_color = lineColor)
-
-
-    # Add all the glyphs to the main plot
-    glyph = Circle(x="x", y="y", size="r", fill_color="colors", line_width=1)
-    plot2.add_glyph(source2, glyph)
-
-    '''
-
-    plot3 = figure(
-        title=None, x_range=xdr, y_range=ydr, plot_width=1100, plot_height=700,
-        h_symmetry=False, v_symmetry=False, min_border=0, toolbar_location = None)
-    plot3.axis.visible = None
-    plot3.xgrid.visible = False
-    plot3.ygrid.visible = False
-
-    plot3.add_tools(HoverTool(
-    tooltips = [
-            ('Sub-Reddit Name', '@name'),
-            ('URL', '@URL'),
-            ]
-        ))
-
-
-    # Add lines connecting each glyph
-    for i in range(N[curPickle]):
-        for j in range(N[curPickle]):
-            if j <= i:
-                rgbval = 255 - 255*connections[i][j] # Shade from white to black based on connection strength
-                lineColor = RGB(rgbval, rgbval, rgbval)
-                width = connections[i][j]
-                x = X[curPickle]; y = Y[curPickle]
-                plot3.line([x[i], x[j]], [y[i], y[j]],
-                    line_width = np.random.randint(7), line_color = lineColor)
-
-
-    # Add all the glyphs to the main plot
-    glyph = Circle(x="x", y="y", size="r", fill_color="colors", line_width=1)
-    plot3.add_glyph(source, glyph)
-    '''
-
-
-    ## Bokeh Widget and JS Code ##############################################
-    '''
-    callback = CustomJS(args=dict(source=source), code="""
-        var data = source.data;
-        var time = time.value;
-        var A = amp.value;
-        var k = freq.value;
-        var phi = phase.value;
-        var B = offset.value;
-        var color = data['colors']
-        var y = data['y']
-        for (var i = 0; i < x.length; i++) {
-            y[i] = B + A*Math.sin(k*x[i]+phi);
-        }
-        source.change.emit();
-    """)
-
-
-
-
-    ##################################################################################
-    pickleMenu = Select(title="Option:", value="foo", options=pickles, callback=callback)
-    callback.args["menu"] = pickleMenu
-    pickleMenu.js_on_change("value", callback)
-
-    timeSlider = Slider(start=0, end=N[curPickle], value=1, step=1,
-                title="Change Time", callback=callback)
-    callback.args["timeSlider"] = timeSlider
-    timeSlider.js_on_change('time', callback)'''
-
-    html_file = open('hacknc2018.html', 'r')
-    text = Div(text = html_file.read(), width = 400, height = 400)
+    memeimages = Div(text = memeimages)
 
     layout = row(
-        #widgetbox(timeSlider, pickleMenu),
-        gridplot([plot0, plot1], [plot2]),
-        text)
+        description,
+        gifs,
+        memeimages
+        )
     show(layout)
 
-def make_color(array2D):
-    colors = []
-    for i in range(len(array2D)):
-        colors.append([])
 
-    for i in range(len(array2D)):
-        for j in range(len(array2D[0])):
-            colors[i] = RGB(255*array2D[i][j], 20, 180*(1-array2D[i][j]))
-
-    return colors
-
-startup()
+main()
