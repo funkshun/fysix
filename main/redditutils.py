465cc03cb09b38b4262fe265ee5d788cf3251fee
Pyrrxa <pyrrxa@gmail.com>
2018-10-06 20:23:15 -0400

    Added connectivity functionality

--- main/redditutils.py
+++ main/redditutils.py
@@ -9,14 +9,19 @@ reddit = praw.Reddit(user_agent='HackNCfysix',
 #Returns a connectivity index by evaluating the number of
 #posters randomly selected who post in both subs over number selected
 
-def connectivity(sub1, sub2, draws):
+def _connectivity(sub1, sub2, draws):
     
     sum_connected = 0
     authors = []
-    
+    if reddit.subreddit(sub1).subscribers > reddit.subreddit(sub2).subscribers:
+        big = sub1
+        small = sub2
+    else:
+        big = sub2
+        small = sub1
     #get sum posts
-    init_posts = reddit.subreddit(sub1).top(limit=draws)
-    authors_checked = 0
+    init_posts = reddit.subreddit(small).top(limit=draws)
+    
     for post in init_posts:
         try:
             #if we have enough unique authors, finish
@@ -28,26 +33,59 @@ def connectivity(sub1, sub2, draws):
             #else, add to authors
             else:
                 authors.append(post.author.name)
-                if len(authors) % 20 == 0:
-                    print(len(authors))
         except:
             pass
     for author in authors:
         user = reddit.redditor(author)
-        authors_checked += 1
-        if authors_checked % 20 == 0:
-            print("Author " + str(authors_checked))
         try:
-            for submission in user.submissions.new(limit=10):
+            for submission in user.submissions.new(limit=draws):
                 y = submission.subreddit.display_name
-                if y == sub2:
+                if y == big:
                     sum_connected += 1
                     break
         except:
             pass
-    return (sum_connected / draws)
+    return 2 * (sum_connected / draws)
+
+def group_connectivity(communities, draws):
+    ret = {}
+    max_conn = 0
+    for i in range(0, len(communities)):
+        for j in range(i, len(communities)):
+            if i == j:
+                pass
+            else:
+                first = communities[i]
+                sec = communities[j]
+                ret[first + ", " + sec] = _connectivity(first, sec, draws)
+                #ret[sec + ", " + sec] = _connectivity(first, sec, draws)
+
+    for k in range(0, len(communities)):
+        for l in range(i, len(communities)):
+            if k == l:
+                pass
+            else:
+                first = communities[k]
+                sec = communities[l]
+                max_conn = max(ret[first + ", " + sec], max_conn)
+                #ret[sec + ", " + sec] = _connectivity(first, sec, draws)
     
-    
+    for l in range(0, len(communities)):
+        for m in range(i, len(communities)):
+            if l == m:
+                pass
+            else:
+                first = communities[l]
+                sec = communities[m]
+                ret[first + ", " + sec] = ret[first + ", " + sec] / max_conn
+                #ret[sec + ", " + sec] = _connectivity(first, sec, draws)
+    return ret
+
+def connectivity(sub1, sub2, conns):
+    return conns[sub1 + ", " + sub2]
+
+
+        
 
 def del_dups(seq):
     seen = {}
