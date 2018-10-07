83b08ca55d8a4f8919fcc63ed6ec5a447d2b3b67
Pyrrxa <pyrrxa@gmail.com>
2018-10-06 23:45:21 -0400

    WE GOT THE PICKLE

--- examples/hits_plotter.py
+++ examples/hits_plotter.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
+import datetime
 import pandas as pd
 import pylab
 import numpy as np
 
 
-df = pd.read_csv('regex_test.csv', sep=',',header=None)
-data = df.values
-binwidth = np.abs(max(data) - min(data))/30
+df = pd.read_csv('mothmemessub.csv', sep=',',header=None)
+raw_data = df.values
+data = []
+for datum in raw_data:
+    data.append(datetime.datetime.fromtimestamp(datum).strftime('%c'))
+
+stddev=np.std(data)
+mean_t = np.mean(data)
+
+data = data[(abs((data - mean_t) / stddev) < 3)]
+
+
+
+binwidth = np.abs(max(data) - min(data))/100
 print(binwidth)
 
 plt.hist(data, bins=np.arange(min(data), max(data) + binwidth, binwidth))
