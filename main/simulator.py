e6b07cc5b3164012f26aecce27fb41e6bb0f4c7d
nickk124 <37184532+nickk124@users.noreply.github.com>
2018-10-07 01:53:13 -0400

    changed pickle file names, changed some simulation parameters

--- main/simulator.py
+++ main/simulator.py
@@ -7,9 +7,9 @@ import pickle
 # INITIALIZATION #
 def initialize(simName): #returns a dict specifying prechosen initial quantities. takes in simulation name out of smallpolitical, political
     #format: 'pickle name' : [picklefilename (str), T, N, M, subnames (list of str), b array, k array, initialization array(N, 3)]
-    masterdict = {}
+    masterlist = []
     if simName == 'smallpolitical':
-        T_init = 100 #total number of time steps
+        T_init = 200 #total number of time steps
         N_init = 5 #total number of communities
         M_init = 100
         h_init = 1
@@ -26,11 +26,11 @@ def initialize(simName): #returns a dict specifying prechosen initial quantities
         simulatingstring = "Trump"
 
         #if I want to initialize explainlikeimfive as being infected with some i, while the other communities as non-infected (so s,i,r = 0):
-        y_init[0] = np.array([0.1, 0.0, 0.0]) # index of Y_init: 0 is the time (so 0 since this is initial), the next index is the index of sub you want to give initial condition
-        y_init[1] = np.array([0.6, 0.4, 0.0])
-        y_init[2] = np.array([0.9, 0.1, 0.0])
-        y_init[3] = np.array([0.2, 0.8, 0.0])
-        y_init[4] = np.array([1.0, 0.0, 0.0])
+        y_init[0] = np.array([1.0, 0.0, 0.0]) # index of Y_init: 0 is the time (so 0 since this is initial), the next index is the index of sub you want to give initial condition
+        y_init[1] = np.array([0.9, 0.1, 0.0])
+        y_init[2] = np.array([0.9, 0.0, 0.0])
+        y_init[3] = np.array([0.5, 0.5, 0.0])
+        y_init[4] = np.array([0.8, 0.2, 0.0])
         #initialize b and k:
 
         #the larger it is, the faster that the subreddit gets infected
@@ -38,14 +38,14 @@ def initialize(simName): #returns a dict specifying prechosen initial quantities
         #the larger it is, the faster that the subreddit recovers
         k_init = np.array([0.1, 0.01, 0.2, 0.4, 0.3]) # usually range from 0.01 to 0.5
 
-        masterdict['smallpolitical'] = {['conn.pk1', T_init, N_init, M_init, subnames_init, b_init, k_init, y_init]}
+        masterlist = ['smallpolitical.pk1', T_init, N_init, M_init, subnames_init, b_init, k_init, y_init]
 
-        return masterdict
+        return masterlist
 
 
     # political #
     if simName == 'political':
-        T_init = 100 #total number of time steps
+        T_init = 200 #total number of time steps
         N_init = 15 #total number of communities
         M_init = 100
         h_init = 1
@@ -73,47 +73,47 @@ def initialize(simName): #returns a dict specifying prechosen initial quantities
         simulatingstring = "Trump"
 
         #if I want to initialize explainlikeimfive as being infected with some i, while the other communities as non-infected (so s,i,r = 0):
-        y_init[0] = np.array([0.7, 0.3, 0.0]) # index of Y_init: 0 is the time (so 0 since this is initial), the next index is the index of sub you want to give initial condition
-        y_init[1] = np.array([0.2, 0.8, 0.0])
-        y_init[2] = np.array([0.9, 0.1, 0.0])
-        y_init[3] = np.array([0.8, 0.2, 0.0])
+        y_init[0] = np.array([1.0, 0.0, 0.0]) # index of Y_init: 0 is the time (so 0 since this is initial), the next index is the index of sub you want to give initial condition
+        y_init[1] = np.array([1.0, 0.0, 0.0])
+        y_init[2] = np.array([1.0, 0.0, 0.0])
+        y_init[3] = np.array([1.0, 0.0, 0.0])
         y_init[4] = np.array([1.0, 0.0, 0.0])
-        y_init[5] = np.array([0.5, 0.5, 0.0])
-        y_init[6] = np.array([0.3, 0.7, 0.0])
-        y_init[7] = np.array([0.4, 0.6, 0.0])
+        y_init[5] = np.array([1.0, 0.0, 0.0])
+        y_init[6] = np.array([1.0, 0.0, 0.0])
+        y_init[7] = np.array([1.0, 0.0, 0.0])
         y_init[8] = np.array([1.0, 0.0, 0.0])
         y_init[9] = np.array([1.0, 0.0, 0.0])
         y_init[10] = np.array([0.6, 0.4, 0.0])
-        y_init[11] = np.array([0.8, 0.2, 0.0])
-        y_init[12] = np.array([0.7, 0.3, 0.0])
-        y_init[13] = np.array([0.9, 0.1, 0.0])
+        y_init[11] = np.array([1.0, 0.0, 0.0])
+        y_init[12] = np.array([1.0, 0.0, 0.0])
+        y_init[13] = np.array([1.0, 0.0, 0.0])
         y_init[14] = np.array([1.0, 0.0, 0.0])
         #initialize b and k:
 
         #the larger it is, the faster that the subreddit gets infected
-        b_init = np.array([0.8, 1.5, 0.5, 1.3, 0.5, 1.2, 1.6, 1.4, 1.1, 0.5, 0.8, 0.4, 0.9, 1.1, 0.3]) #usualy range from k to 2
+        b_init = np.array([0.8, 1.5, 0.2, 0.4, 0.3, 0.7, 0.8, 1.4, 0.3, 0.1, 0.5, 0.3, 0.6, 0.9, 0.2]) #usualy range from k to 2
         #the larger it is, the faster that the subreddit recovers
-        k_init = np.array([0.1, 0.01, 0.2, 0.4, 0.3, 0.4, 0.05, 0.1, 0.2, 0.2, 0.6, 0.3, 0.4, 0.6, 0.2]) # usually range from 0.01 to 0.5
+        k_init = np.array([0.05, 0.01, 0.1, 0.1, 0.09, 0.08, 0.09, 0.08, 0.03, 0.07, 0.1, 0.07, 0.05, 0.04, 0.1]) # usually range from 0.01 to 0.1
 
-        masterdict['political'] = {['15byconn.pk1', T_init, N_init, M_init, subnames_init, b_init, k_init, y_init]}
+        masterlist = ['political.pk1', T_init, N_init, M_init, subnames_init, b_init, k_init, y_init]
 
-        return masterdict
+        return masterlist
 #format: 'pickle name' : [picklefilename (str), T, N, M, subnames (list of str), b array, k array, initialization array(N, 3)]
 
 # SIMULATOR #
 def simulate(simName): # Y0, T, N, h, M, b, k, subnames
-    masterdict = initialize(simName)
+    masterlist = initialize(simName)
 
-    picklefilename = masterdict[0]
-    T = = masterdict[1]
-    N = masterdict[2]
-    M = masterdict[3]
-    subnames = masterdict[4]
-    b = masterdict[5]
-    k = masterdict[6]
+    picklefilename = masterlist[0]
+    T = masterlist[1]
+    N = masterlist[2]
+    M = masterlist[3]
+    subnames = masterlist[4]
+    b = masterlist[5]
+    k = masterlist[6]
 
     Y0 = np.zeros((T,N,3))  #initial data for s, i, r for each community. It's an array of T arrays that have N arrays that each of the 3 values of s, i ,r
-    Y0[0] = masterdict[7]
+    Y0[0] = masterlist[7]
     Y = Y0
 
     t = 0 #initial time step
@@ -128,7 +128,7 @@ def simulate(simName): # Y0, T, N, h, M, b, k, subnames
         for e in range(N):
             if d > e:
                 L[d,e] = L[e,d]
-    print(L)
+    #print(L)
     #TEST:
     #L = np.random.uniform(0.2, 0.7, (N,N))
     #print(L)
@@ -173,15 +173,21 @@ def simulate(simName): # Y0, T, N, h, M, b, k, subnames
         final[subnames[j]] = final_i
     return final
 
-x = main(Y_init, T_init, N_init, h_init, M_init, b_init, k_init, subnames_init)
+x = simulate("smallpolitical")
 #print(x)
 
+T_test = 200
+t = np.arange(T_test)
 
-t = np.arange(T_init)
+subnames_test = ["esist",
+"The_Mueller",
+"liberal",
+"politics",
+"neoliberal"]
 
 plt.figure(1)
 
-for name in subnames_init:
+for name in subnames_test:
     x1 = np.array(x[name])
     plt.plot(t, x1, c=np.random.rand(3,), label = name)
 
